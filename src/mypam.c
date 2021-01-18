7bf46e4dc6dea7cfbeafff5c99f23669be58454f
Sergio García <garcapradossergi76@gmail.com>
2021-01-18 22:59:05 +0100

    Leave only pam_sm_authenticate function

--- src/mypam.c
+++ src/mypam.c
@@ -3,22 +3,9 @@
 #include <string.h>
 #include <security/pam_appl.h>
 #include <security/pam_modules.h>
-#include <sys/stat.h>
-#define FILE "/etc/ssh/sshd_config"
 
-/* expected hook */
-PAM_EXTERN int pam_sm_setcred( pam_handle_t *pamh, int flags, int argc, const char **argv ) {
-	return PAM_SUCCESS;
-}
-
-PAM_EXTERN int pam_sm_acct_mgmt(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-	return PAM_SUCCESS;
-}
-
-/* expected hook, this is where custom stuff happens */
 PAM_EXTERN int pam_sm_authenticate( pam_handle_t *pamh, int flags,int argc, const char **argv ) {
 	int retval;
-	struct stat buffer;
 
 	const char* pUsername;
 	retval = pam_get_user(pamh, &pUsername, "Username: ");
