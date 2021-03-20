24050e5270485711407f7b5e7a940bbbc1388932
Sergio García <garcapradossergi76@gmail.com>
2021-03-21 00:54:05 +0100

    Change sha to crypt

---
+++ lib/crypt.h
@@ -0,0 +1,8 @@
+#ifndef SHA_H_
+#define SHA_H_
+
+char *sha512(const char *digest);
+const int get_challenge_size();
+char *get_challenge();
+
+#endif
---
+++ src/crypt.c
@@ -0,0 +1,67 @@
+/*
+Doc: https://www.gnu.org/software/libc/manual/html_mono/libc.html#Unpredictable-Bytes
+https://www.openssl.org/docs/manmaster/man3/SHA512.html
+*/
+
+#include <stdio.h>
+#include <openssl/sha.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#define CHALLENGE_SIZE 64
+
+char *sha512(const char *digest)
+{
+    static char buffer[128];
+    if (digest == NULL)
+    {
+        fprintf(stderr, "Digest cannot be empty\n");
+
+    }
+    else 
+    {
+        SHA512_CTX sha512;
+        unsigned char hash[SHA512_DIGEST_LENGTH];
+        
+        SHA512_Init(&sha512);
+        SHA512_Update(&sha512, digest, strlen(digest));
+        SHA512_Final(hash, &sha512);
+        
+        int i = 0;
+        for (i = 0; i < SHA512_DIGEST_LENGTH; i++)
+        {
+            sprintf(buffer + (i * 2), "%02x", hash[i]);
+        }
+    }
+    
+    return buffer;
+}   
+
+const int get_challenge_size()
+{
+    return CHALLENGE_SIZE;
+}
+
+char *get_challenge()
+{
+    unsigned char ubytes[CHALLENGE_SIZE];
+    char *challenge = malloc(CHALLENGE_SIZE);
+    const char *const alphanum =
+        "./0123456789ABCDEFGHIJKLMNOPQRST"
+        "UVWXYZabcdefghijklmnopqrstuvwxyz";
+    int i;
+
+    /* Retrieve 16 unpredictable bytes from the operating system. */
+    if (getentropy(ubytes, CHALLENGE_SIZE))
+    {
+        perror("getentropy");
+    }
+
+    /* Use them to fill in the salt string. */
+    for (i = 0; i < CHALLENGE_SIZE-1; i++)
+        challenge[i] = alphanum[ubytes[i] & 0x3f];
+    challenge[i] = '\0';
+    
+    return challenge;
+}
