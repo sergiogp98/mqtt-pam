7f1b261a66b3d292712b3a0aaece0bf54eee0325
Sergio García Prados <garcapradossergi76@gmail.com>
2021-05-01 12:33:35 +0200

    Change lib to include

--- lib/utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef UTILS_H_
-#define UTILS_H_
-
-void set_id(char buffer[], const int size, const char *id);
-void set_topic(char buffer[], const int size, const char *src, const char *dst, const char *item);
-void set_buffer(char buffer[], const int size, const char *dgst);
-
-#endif
